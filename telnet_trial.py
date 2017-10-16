749f692ea3fe34c5c3c0892047bf0b82c34b374f
Alan Ehrle <31059918+ehcap1@users.noreply.github.com>
2017-10-15 21:47:17 -0700

    Update telnet_trial.py

--- telnet_trial.py
+++ telnet_trial.py
@@ -1,11 +1,24 @@
 # telnet program example
+import RPi.GPIO as GPIO
 import socket, select, string, sys, time, random
+
+
+Btn1Pin = 11    # pin11 fuel pump off or on
+Btn2Pin = 12    # pin12 bcn on or off
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+    GPIO.setup(Btn1Pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)   # Set LedPin's mode is input
+    GPIO.setup(Btn2Pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # Set BtnPin's mode is input, and pull up to high level(3.3V)
+
+#subscribing to flaps, beacon, and fuel pump 1
+messages = ["sub sim/flightmodel/controls/flaprqst\n", "sub sim/cockpit2/switches/beacon_on\n", "sub sim/cockpit/engine/fuel_pump_on 1\n"]
 
 #main function
 if __name__ == "__main__":
-
-    host = input("type host name: ")
-    port = input("which port would you like to use? ")
+    setup()
+    host = "" #input("type host name: ")#change the ip you are trying to access 
+    port = 51000   #int(input("which port would you like to use? "))
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(2)
@@ -20,9 +33,10 @@ if __name__ == "__main__":
 
 
 
-    msg = 'sub sim/flightmodel/controls/flaprqst\n'
-    s.send(msg)
-    time.sleep(0.5)
+    for msg in messages:
+     #python 3 str.encode('')
+        s.send(msg) #bytes(msg, 'utf-8'))
+        time.sleep(0.5)
 
 
     while 1:
@@ -34,10 +48,51 @@ if __name__ == "__main__":
         for sock in read_sockets:
             if sock == s:
                     data = sock.recv(4096)
-                    sys.stdout.write(data)
-#*****test done to see if program could change controls from pi to flightsim on other computer******
+
+                    sys.stdout.write(str(data))
+
+        if (GPIO.input(Btn1Pin) == GPIO.HIGH):
+            msg = ("set sim/cockpit/engine/fuel_pump_on [1,2,0,0,0,0,0,0]\n")
+            s.send(msg)
+            time.sleep(0.2)
+
+
+
+        else:
+            msg = ("set sim/cockpit/engine/fuel_pump_on [0,2,0,0,0,0,0,0]\n")
+            s.send(msg)
+            time.sleep(0.2)
+
+        if (GPIO.input(Btn2Pin) == GPIO.HIGH) :
+            # Check whether the button is pressed or not.
+
+            msg = ('set sim/cockpit2/switches/beacon_on 1\n')
+
+            s.send(msg)
+            time.sleep(0.2)
+
+        else:
+            msg = ('set sim/cockpit2/switches/beacon_on 0\n')
+            s.send(msg)
+            time.sleep(0.2)
+
+
+
+
+
+
+
+
+
+           #msg = "set sim/flightmodel/controls/flaprqst 0\n"
+            #s.send(msg)
+
+
         #time.sleep(0.5)
-        #msg = 'set sim/flightmodel/controls/flaprqst '+str(random.random())+'\n'
-        #s.send(msg)
+        # msg = 'set sim/flightmodel/controls/flaprqst '+str(random.random())+'\n'
+        # s.send(msg)
+
+
+
 
 
